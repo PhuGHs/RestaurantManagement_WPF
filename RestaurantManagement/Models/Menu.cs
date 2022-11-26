d988ad442ba640b44a50e2300ff42d6839dc660a
Phu Le <21522466@gm.uit.edu.vn>
2022-11-26 17:18:57 +0700

    add order feature and add some dummy dishes to menuitem

--- RestaurantManagement/Models/Menu.cs
+++ RestaurantManagement/Models/Menu.cs
@@ -16,14 +16,14 @@ namespace Menu.Models
         private string foodName;
         private Decimal price;
         private string foodImage;
-        public int ID { get { return id; } set { id = value; } }    
+        public int ID { get { return id; } set { id = value; } }
 
         public string FoodName
         {
             get { return foodName; }
             set
             {
-                if(foodName != value)
+                if (foodName != value)
                 {
                     foodName = value;
                     OnPropertyChanged("food name");
@@ -34,7 +34,7 @@ namespace Menu.Models
         public Decimal Price
         {
             get { return price; }
-            set 
+            set
             {
                 if (price != value)
                 {
@@ -62,6 +62,68 @@ namespace Menu.Models
             get { return String.Format("{0:0,0 VND}", Price); }
         }
 
-        
+
+    }
+    public class SelectedMenuItem : BaseViewModel
+    {
+        public SelectedMenuItem(int ID, string foodName, Decimal price, int quantity)
+        {
+            _id = ID;
+            _foodName = foodName;
+            _price = price;
+            _quantity = quantity;
+        }
+        #region attributes
+        private int _id;
+        private string _foodName;
+        private Decimal _price;
+        private int _quantity;
+        #endregion
+        #region properties
+        public int ID { get { return _id; } set { _id = value; } }
+
+        public string FoodName
+        {
+            get { return _foodName; }
+            set
+            {
+                if (_foodName != value)
+                {
+                    _foodName = value;
+                    OnPropertyChanged("food name");
+                }
+            }
+        }
+
+        public Decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    OnPropertyChanged("price");
+                }
+            }
+        }
+
+        public string PriceVNDCurrency
+        {
+            get { return String.Format("{0:0,0 VND}", Price); }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged("quantity");
+                }
+            }
+        }
+        #endregion
     }
 }
--- RestaurantManagement/ViewModel/MenuViewModel.cs
+++ RestaurantManagement/ViewModel/MenuViewModel.cs
@@ -11,6 +11,80 @@ namespace QuanLyNhaHang.ViewModel
 {
     public class MenuViewModel : BaseViewModel
     {
-        
+        public MenuViewModel()
+        {
+            OrderFeature = new RelayCommand<MenuItem>((p) => true, (p) => OrderAnItem(p.ID));
+            _menuItems = new ObservableCollection<MenuItem>();
+            _selectedItems = new ObservableCollection<SelectedMenuItem>();
+            LoadMenuItems();
+        }
+
+        #region attributes
+        private ObservableCollection<MenuItem> _menuItems;
+        private ObservableCollection<SelectedMenuItem> _selectedItems;
+        #endregion
+
+        #region properties
+        public ObservableCollection<MenuItem> MenuItems { get { return _menuItems; } set { _menuItems = value; } }
+        public ObservableCollection<SelectedMenuItem> SelectedItems { get { return _selectedItems; } set { _selectedItems = value; } }
+        #endregion
+
+        #region commands
+        public ICommand OrderFeature { get; set; }  
+        #endregion
+
+        #region methods
+        private void LoadMenuItems()
+        {
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_1.png", ID = 1, FoodName = "Phở Bò", Price = 45000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_2.png", ID = 2, FoodName = "Canh Chua", Price = 20000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_3.png", ID = 3, FoodName = "Lẩu Hải Sản", Price = 140000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_4.jpg", ID = 4, FoodName = "Mì tôm", Price = 10000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_5.jpg", ID = 5, FoodName = "Hàu nướng", Price = 200000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 6, FoodName = "Cá Ngừ", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 7, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 8, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 10, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 11, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 12, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            _menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 13, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+
+            MenuItems = _menuItems;
+        }
+
+        private void OrderAnItem(int ID)
+        {
+            foreach(MenuItem item in _menuItems)
+            {
+                if (item.ID == ID)
+                {
+                    SelectedMenuItem x = checkIfAnItemIsInOrderItems(ID);
+                    if (x != null)
+                    {
+                        x.Quantity++;
+                        return;
+                    }
+                    SelectedMenuItem s_item = new SelectedMenuItem(item.ID, item.FoodName, item.Price, 1);
+                    SelectedItems.Add(s_item);  
+                    break;
+                }
+            }
+        }
+        #endregion
+
+        #region complementary methods
+        private SelectedMenuItem checkIfAnItemIsInOrderItems(int ID)
+        {
+            foreach(SelectedMenuItem item in _selectedItems)
+            {
+                if (item.ID == ID)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        #endregion
     }
 }
