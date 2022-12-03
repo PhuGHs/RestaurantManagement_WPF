df2a052248f2026b0f77c8948a17964e508058cb
Phu Le <21522466@gm.uit.edu.vn>
2022-12-03 22:17:22 +0700

    add temporary db to UserSetting

--- RestaurantManagement/Models/NhanVien.cs
+++ RestaurantManagement/Models/NhanVien.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace QuanLyNhaHang.Models
 {
-    internal class NhanVien
+    public class NhanVien
     {
+        private string _MaNV;
+        public string MaNV { get => _MaNV; set => _MaNV = value; }
+        private string _HoTen;
+        public string HoTen { get => _HoTen; set => _HoTen = value; }
+        private string _ChucVu;
+        public string ChucVu { get => _ChucVu; set => _ChucVu = value; }
+        private string _DiaChi;
+        public string DiaChi { get => _DiaChi; set => _DiaChi = value; }
+        private bool _Fulltime;
+        public bool Fulltime { get => _Fulltime; set => _Fulltime = value; }
+        private string _SDT;
+        public string SDT { get => _SDT; set => _SDT = value; }
+        private string _NgayVaoLam;
+        public string NgayVaoLam { get => _NgayVaoLam; set => _NgayVaoLam = value; }
+        private string _NgaySinh;
+        public string NgaySinh { get => _NgaySinh; set => _NgaySinh = value; }
+        private string _TaiKhoan;
+        public string TaiKhoan { get => _TaiKhoan; set => _TaiKhoan = value; }
+        private string _MatKhau;
+        public string MatKhau { get => _MatKhau; set => _MatKhau = value; }
+        public NhanVien(string id, string ten, string chucvu, string diachi, bool fulltime, string sdt, string ngayvl, string ngsinh, string tk, string mk)
+        {
+            MaNV = id;
+            HoTen = ten;
+            ChucVu = chucvu;
+            DiaChi = diachi;
+            Fulltime = fulltime;
+            SDT = sdt;
+            NgayVaoLam = ngayvl;
+            NgaySinh = ngsinh;
+            TaiKhoan = tk;
+            MatKhau = mk;
+        }
     }
 }
--- RestaurantManagement/ViewModel/CaiDatViewModel.cs
+++ RestaurantManagement/ViewModel/CaiDatViewModel.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLyNhaHang.Models;
 
 namespace QuanLyNhaHang.ViewModel
 {
     public class CaiDatViewModel : BaseViewModel
     {
+        public CaiDatViewModel()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["QuanLyNhaHang"].ToString();
+            OpenConnection();
+            ReadData();
+        }
+        private NhanVien _nhanVien;
+        public NhanVien NhanVien { get { return _nhanVien; } set { _nhanVien = value; OnPropertyChanged(); } }
+        private SqlConnection SqlCon = null;
+        private string connectionString;
+        private void OpenConnection()
+        {
+            SqlCon = new SqlConnection(connectionString);
+            if(SqlCon.State == ConnectionState.Closed)
+            {
+                SqlCon.Open();
+            }
+        }
+        private void ReadData()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select * from NhanVien";
+            cmd.Connection = SqlCon;
+            
+            SqlDataReader read = cmd.ExecuteReader();
+            while(read.Read())
+            {
+                _nhanVien = new NhanVien(read.GetInt16(0).ToString(), read.GetString(1), read.GetString(2), read.GetString(3), read.GetBoolean(4), read.GetString(7), read.GetDateTime(8).ToString(), read.GetDateTime(9).ToString(), read.GetString(5), read.GetString(6));
+                //_nhanVien.MaNV = read.GetInt32(0).ToString();
+                //_nhanVien.HoTen = read.GetString(1);
+                //_nhanVien.ChucVu = read.GetString(2);
+                //_nhanVien.DiaChi = read.GetString(3);
+                //_nhanVien.Fulltime = read.GetBoolean(4);
+                //_nhanVien.TaiKhoan = read.GetString(5);
+                //_nhanVien.MatKhau = read.GetString(6);
+                //_nhanVien.SDT = read.GetString(7);
+                //_nhanVien.NgayVaoLam = read.GetDateTime(8).ToString();
+                //_nhanVien.NgaySinh = read.GetDateTime(9).ToString();
+            }
+            CloseConnection();
+        }
+        private void CloseConnection()
+        {
+            if(SqlCon.State == ConnectionState.Open)
+            {
+                SqlCon.Close();
+            }
+        }
     }
 }
