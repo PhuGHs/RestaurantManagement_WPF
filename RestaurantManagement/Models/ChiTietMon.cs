2417947dd35576241cbdfde0db36ab64af7acdb0
Phu Le <21522466@gm.uit.edu.vn>
2023-01-06 16:27:39 +0700

    add MenuAdmin_ThemNLieu UI

---
+++ RestaurantManagement/Models/ChiTietMon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyNhaHang.ViewModel;
+
+namespace QuanLyNhaHang.Models
+{
+    public class ChiTietMon : BaseViewModel
+    {
+        private string _maMon;
+        private string _tenNL;
+        private int _soluong;
+
+        public string MaMon { get { return _maMon; } set { _maMon = value; OnPropertyChanged(); } }
+        public string TenNL { get { return _tenNL; } set { _tenNL = value; OnPropertyChanged(); } }
+        public int SoLuong { get { return _soluong; } set { _soluong = value; OnPropertyChanged(); } }
+        public ChiTietMon(string TenNL, string MaMon, int SoLuong = 0)
+        {
+            _maMon = MaMon;
+            _tenNL = TenNL;
+            _soluong = SoLuong;
+        }
+    }
+}
